60423a52825f2cf778a5a8b14edaaf082acdf992
leonawicz <matt.rstat@gmail.com>
2017-10-11 10:56:21 -0600

    Refactor use_apps, add arguments for showcase mode. Add use_appdesc and use_appreadme functions. Add and update documentation and export functions.

--- R/snapapps.R
+++ R/snapapps.R
@@ -2,7 +2,7 @@
 #'
 #' Include Shiny apps from a local clone of the SNAP shiny-apps repository in the local clone of the snapapps package.
 #'
-#' This function will check to see if local Git repositories exist for both the \code{snapapps} pacakge and the SNAP \code{shiny-apps} repository.
+#' This function will check to see if local Git repositories exist for both the \code{snapapps} package and the SNAP \code{shiny-apps} repository.
 #' If \code{id} is a valid app directory in the latter, it will be copied to the former. \code{id} may be a vector.
 #' This function should be used once per included app. It should only be reused (to update an app) if changes have not been made
 #' to the copy previously placed in \code{snapapps} or you will lose those changes.
@@ -15,13 +15,15 @@
 #'
 #' @param id character, app directory name.
 #' @param base_path base file path.
+#' @param description logical, add a \code{DESCRIPTION} file template for showcase mode.
+#' @param readme logical, add a \code{Readme.md} template for showcase mode.
 #' @param overwrite logical, overwrite previously added apps.
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{use_apps(id = "rvdist")}
-use_apps <- function(id, base_path = ".", overwrite = FALSE){
+use_apps <- function(id, base_path = ".", description = TRUE, readme = TRUE, overwrite = FALSE){
   x <- "../shiny-apps"
   y <- "inst/shiny"
   if(!file.exists("../shiny-apps"))
@@ -42,6 +44,8 @@ use_apps <- function(id, base_path = ".", overwrite = FALSE){
   purrr::walk(id, ~(if(!.x %in% cur_files || overwrite){
     cat(paste("Copying app to:", file.path(path, .x), "\n"))
     file.copy(file.path(x, id), path, recursive = TRUE)
+    if(description) use_appdesc(path)
+    if(readme) use_appreadme(path)
     })
   )
   purrr::walk(id, ~(if(!.x %in% cur_files || overwrite){
@@ -49,5 +53,69 @@ use_apps <- function(id, base_path = ".", overwrite = FALSE){
       unlink(file.path(path, .x, "rsconnect"), recursive = TRUE, force = TRUE)
     })
   )
+  invisible()
+}
+
+#' Create an app DESCRIPTION file
+#'
+#' Add a DESCRIPTION template to a Shiny app.
+#'
+#' This file is used with Shiny app showcase mode.
+#' Since \code{snapmeta} is a developer package used by the SNAPverse author/maintainer,
+#' most arguments to \code{use_appdesc} have contextual defaults that you must otherwise override.
+#' \code{title} and \code{tags} do not have defaults. If missing, they should be updated directly in the generated DESCRIPTION file.
+#' Tags passed in the form \code{tags = c("random variables", "plotmath")} will result in \code{random-variables, plotmath}.
+#'
+#' @param base_path output directory.
+#' @param title app title.
+#' @param author author name.
+#' @param url author url.
+#' @param license license type.
+#' @param mode Shiny display mode.
+#' @param tags optional tags. May be a vector. See details.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{use_appdesc()}
+use_appdesc <- function(base_path = ".", title, author = "Matthew Leonawicz",
+                        url = "https://leonawicz.github.io/", license = "MIT",
+                        mode = "Showcase", tags){
+  x <- readLines(file.path(system.file(package = "snapmeta"), "resources/apps/DESCRIPTION"))
+  if(!missing(title)) x <- gsub("__title__", title, x)
+  if(!missing(tags)){
+    if(!inherits(tags, "character")) stop("`tags` must be a character vector.")
+    tags <- paste(gsub(" ", "-", tags), collapse = ", ")
+    x <- gsub("__tags__", tags, x)
+  }
+  x <- gsub("__author__", author, x)
+  x <- gsub("__authorurl__", url, x)
+  x <- gsub("__license__", license, x)
+  x <- gsub("__displaymode__", mode, x)
+  x <- paste0(paste(x, collapse = "\n"), "\n")
+  sink(file.path(base_path, "DESCRIPTION"))
+  x
+  sink()
+  cat("Added DESCRIPTION file template.\n")
+  invisible()
+}
+
+#' Create an app Readme.md file
+#'
+#' Add a Readme.md template to a Shiny app.
+#'
+#' This file is used to add a text description below a Shiny app when run in showcase mode.
+#'
+#' @param base_path output directory.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{use_appreadme()}
+use_appreadme <- function(base_path = "."){
+  sink(file.path(base_path, "Readme.md"))
+  cat("This Shiny app...\n")
+  sink()
+  cat("Added Readme.md template.\n")
   invisible()
 }
